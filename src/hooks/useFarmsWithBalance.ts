e60f9e55ed0fd8131cb2b488146bebde9221bc09
Mark Evans <president@worldvaporexpo.com>
2021-10-02 03:17:56 -0400

    checkpoint

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingCake',
+        name: 'pendingCrystal',
         params: [farm.pid, account],
       }))
 
